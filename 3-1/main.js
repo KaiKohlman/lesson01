5bff08a805e7a1c850ed447a0768dabc1e4c993c
KaiKohlman <kaikohlman@gmail.com>
2021-06-05 20:20:06 +0900

    九九表を表示してみよう

---
+++ 3-1/main.js
@@ -0,0 +1,15 @@
+
+// for(i=1;arr<=9;arr++){
+//   for(j=1;j<=9;j++){
+//   }
+//   console.log(i*j);
+// }
+
+for(let i = 1;i <= 9;i ++){
+  let arr =[];
+    for(let j=1;j<=9;j++){
+      arr += i*j
+      arr += ' '
+    }
+    console.log(arr);
+}
