05b9822641061e7859877d490234df0d0f449556
KaiKohlman <kaikohlman@gmail.com>
2021-06-15 17:33:33 +0900

    コマンドライン引数で5つの整数値を入力させ、5つの値のうち3番目に大きい値を表示するプログラムを作成せよ

---
+++ 4-1/main.js
@@ -0,0 +1,11 @@
+// コマンドライン引数で5つの整数値を入力させ、5つの値のうち3番目に大きい値を表示するプログラムを作成せよ
+const all = [process.argv[2],process.argv[3],process.argv[4],process.argv[5],process.argv[6]];
+
+function compare(a, b){
+  return a - b;
+}
+
+all.sort(compare);
+console.log(all[2]);
+
+
