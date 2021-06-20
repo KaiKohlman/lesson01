ae2bcaa2b68851173e8be66b3c44db4741755c72
KaiKohlman <kaikohlman@gmail.com>
2021-06-20 16:59:24 +0900

    修正しました

--- 4-2/main.js
+++ 4-2/main.js
@@ -1,14 +1,17 @@
 // 1から50までの数字を表示する。ただし3と3の倍数のときだけWOWと表示する
 
+
 for(i=1;i<=50;i++){
   const result = [];
-  if(i%3===0){
+  if(i%3===0||String(i).match(/3/)){
     result.push('WOW');
-    }else if(String(i).match(/3/)){
-      result.push('WOW');
     }else{
       result.push(i);
     }
     console.log(result);
   }
 
+
+// for(i=1;i<=50;i++){
+
+// }
