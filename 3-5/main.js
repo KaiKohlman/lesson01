4bc697b8ba4206bee81e777dd8e41ff3d55686fd
KaiKohlman <kaikohlman@gmail.com>
2021-06-20 14:33:16 +0900

    dosuu消せた

--- 3-5/main.js
+++ 3-5/main.js
@@ -1,9 +1,5 @@
-// 分布表
-// 15人の生徒のテストの点数の分布表を作って下さい
-
 const students = [17,38,100,95,23,62,77,45,69,81,83,51,42,36,60];
 const shishagonyuu = [];
-const dosuu = [10,20,30,40,50,60,70,80,90,100];
 const devide = [];
 const result = [];
 let sum = 0;
@@ -17,9 +13,9 @@ for(i=0;i<students.length;i++){
 // console.log(shishagonyuu);[ 10, 30, 100, 90, 20, 60, 70, 40,  60, 80, 80, 50,40, 30, 60]
 
 
-for (let i = 0; i < dosuu.length; i++) {
+for (let i = 10; i <=100; i+=10) {
   for (let j = 0; j < shishagonyuu.length; j++) {
-    if (dosuu[i]===shishagonyuu[j]) {
+    if (i===shishagonyuu[j]) {
       sum++;
     }
   }
@@ -31,7 +27,7 @@ for (let i = 0; i < dosuu.length; i++) {
 //分ける
 const max = Math.max(...devide);
   for(let i = 0;i < max; i++){
-    for(let j = 0;j < dosuu.length; j++){
+    for(let j = 0;j <= 10; j++){
       if(devide[j] >= max-i){
         result.push(' * ')
       }else{
