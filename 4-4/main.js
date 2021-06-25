abadc81bbdbca1cedba4dfbffc412927e91de770
KaiKohlman <kaikohlman@gmail.com>
2021-06-25 18:47:32 +0900

    並びはランダムで(0~9)までの全ての数字が入った要素が10個の配列を作成して下さい。

--- 4-4/main.js
+++ 4-4/main.js
@@ -1,10 +1,11 @@
 // 並びはランダムで(0~9)までの全ての数字が入った要素が10個の配列を作成して下さい。
 
 let result = [];
-for(let i = 0; i <= 9; i ++){
-  result += Math.floor((Math.random()*10));
+for(let i = 0; result.length <= 18; i ++){
+  let number = Math.floor((Math.random()*10));
+  if(!result.includes(number)){
+  result += number
   result += ' '
+  }
 }
 console.log(result);
-
-
