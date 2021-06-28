6df51d631ddf0624b51c7b9d9249c6c22dbbd630
KaiKohlman <kaikohlman@gmail.com>
2021-06-28 11:04:42 +0900

    コマンドライン引数で渡された数を小さい順に並べた配列を生成してconsole.log()で出力してください。ただしArray型のメソッドを使うことを禁止します(push()を除く)。入力される数値は0以上9999以下とします。

--- 4-5/main.js
+++ 4-5/main.js
@@ -1,36 +1,27 @@
 // コマンドライン引数で渡された数を小さい順に並べた配列を生成してconsole.log()で出力してください。ただしArray型のメソッドを使うことを禁止します(push()を除く)。入力される数値は0以上9999以下とします。
 
-const array1 =[];
+const array =[];
 const result = [];
 
 for (i = 2;i < process.argv.length; i ++){
   let number = process.argv[i];
-  array1.push(number);
+  array.push(Number(number));
 }
 
-// console.log(array1);
+let max = array[0];
+let min = array[0];
 
-let change = []
-for(i = 0; i < array1.length; i ++){
-  change.push(Number(array1[i]));
-}
-
-// console.log(change);
-
-let max = change[0];
-let min = change[0];
-
-for(i = 0 ; i <= change.length ; i ++){
-  if(change[i] > max){
-    max = change[i];
-  }else if(change[i] < min){
-  min = change[i];
+for(i = 0 ; i <= array.length ; i ++){
+  if(array[i] > max){
+    max = array[i];
+  }else if(array[i] < min){
+  min = array[i];
   }
 }
 
 for(let i = min; i <= max; i ++){
-  for(let j = 0; j <= change.length; j ++){
-    if(i == change[j]){
+  for(let j = 0; j <= array.length; j ++){
+    if(i == array[j]){
       result.push(i);
     }
   }
