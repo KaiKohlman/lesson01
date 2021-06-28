a043d9636e7b8a1117c400b7c936eb6c61c8847e
KaiKohlman <kaikohlman@gmail.com>
2021-06-28 10:55:50 +0900

    コマンドライン引数で指定された時刻を表す長さ 5 の文字列 S が “XX:XX” の形式で与えられます。入力された時刻を時と分に分け、45分後の時と分を表示してください。

--- 4-6/main.js
+++ 4-6/main.js
@@ -1,17 +1,14 @@
 // コマンドライン引数で指定された時刻を表す長さ 5 の文字列 S が “XX:XX” の形式で与えられます。入力された時刻を時と分に分け、45分後の時と分を表示してください。
-let array1 = []
-array1 += process.argv.slice('2');
-let array2 = array1.split(':');
-let index = array2.indexOf(':')
-let array3 = array2.map(Number)
-// console.log(array3);
 
-let hour = array3[0]
-let minutes = array3[1]+45;
+let array1 = process.argv[2].split(':');
+let array2 = array1.map(Number)
+
+let hour = array2[0]
+let minutes = array2[1]+45;
 
 if(minutes >= 60){
   minutes = Math.abs(minutes - 60);
-  hour =array3[0]+1;
+  hour =array2[0]+1;
 }
 
 if(hour >= 24){
